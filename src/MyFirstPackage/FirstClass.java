3ab189f4eafc532caef01dc9a9598757e7d0e9a0
nikkiumar1993 <56229873+nikkiumar1993@users.noreply.github.com>
2020-02-10 22:22:00 -0600

    change

--- src/MyFirstPackage/FirstClass.java
+++ src/MyFirstPackage/FirstClass.java
@@ -3,5 +3,6 @@ package MyFirstPackage;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hey there");
     }
 }
